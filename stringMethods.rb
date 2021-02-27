32cb44ace742b62253b76de62436f699093fafe9
Harsh <harsh@123.com>
2021-02-27 11:02:10 +0530

    String methods in ruby

---
+++ stringMethods.rb
@@ -0,0 +1,11 @@
+name = " Harsh Singh "
+puts name.upcase()
+puts name.downcase()
+puts name.strip()
+puts name.length()
+puts name.include? "Si"
+puts name[1]
+puts name[0,5]
+puts name.index("s")
+puts "Ruby".upcase()
+puts "Ruby".include? "b"
